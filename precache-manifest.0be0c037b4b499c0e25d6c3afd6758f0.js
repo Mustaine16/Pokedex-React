928a1542c1b006e91cc56715f8daa3fa74f9a8e9
eze <ezequieldominguezcp11@gmail.com>
2020-03-10 18:50:02 -0300

    Updates

---
+++ precache-manifest.0be0c037b4b499c0e25d6c3afd6758f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14080f9e624877c629842aff5519f357",
+    "url": "/Pokedex-React/index.html"
+  },
+  {
+    "revision": "158e1b1a69c9813b48e9",
+    "url": "/Pokedex-React/static/css/main.0b26aa4f.chunk.css"
+  },
+  {
+    "revision": "06e541361e88d5fe02cf",
+    "url": "/Pokedex-React/static/js/2.cbde196d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Pokedex-React/static/js/2.cbde196d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "158e1b1a69c9813b48e9",
+    "url": "/Pokedex-React/static/js/main.2b20d8c5.chunk.js"
+  },
+  {
+    "revision": "2daedf6b450136abc034",
+    "url": "/Pokedex-React/static/js/runtime-main.32dca872.js"
+  }
+]);

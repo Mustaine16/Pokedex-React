90a0ee08606ccb410f2c3e6787ab1b5263f49c39
eze <ezequieldominguezcp11@gmail.com>
2020-03-16 17:31:38 -0300

    segundo intento de fix GP

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import Layout from "./components/home/Layout";
 import Home from "./pages/Home";
 import PokemonPage from "./pages/PokemonPage";
-import Prueba from "./components/commons/Prueba";
+
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Layout>
         <Switch>
           <Route exact path={process.env.PUBLIC_URL + '/'} component={Home}></Route>
-          <Route exact path={process.env.PUBLIC_URL + '/:id'} component={PokemonPage}></Route>
+          <Route exact path={process.env.PUBLIC_URL + '/' +':id'} component={PokemonPage}></Route>
         </Switch>
       </Layout>
     </BrowserRouter>
